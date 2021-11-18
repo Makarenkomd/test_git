92ade3f3bdc97e18914b724e77a645512956ecde
makar_md <mahadrum@yandex.ru>
2021-11-18 18:11:15 +0300

    add commit hash

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def print_hi(name):
     print(f'Hi, {name}')
     print("Я смог сделать commit")
-
+    # 6cabb8387620edea1388cb6df2493d0a296a68aa
 
 if __name__ == '__main__':
     print_hi('PyCharm')
